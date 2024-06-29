844e0ed45af456922caa8eccdf029176f44059dd
AKARSH <akarsh.a@spericorn.com>
2024-06-29 11:51:51 +0530

    Basic App complete

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 // Create a server
 const http = require('http');
 
-// server port 3001 listen
-// Listen to incoming request
-// Send back response
+const requestHandler = require('./routes');
 
-const requestHandler = (req, res) => {
-  const method = req.method;
-  const url = req.url;
+// GET /add-product FORM Input Field , Button, On Submit
+// On submit - Text File write - / Url redirect
 
-  if (url == '/' && method == 'GET') {
-    res.write('<html><head><title>Product Listing Page</title></head>');
-    res.write('<body><h1>Product Listing Page</h1></body></html>');
-    res.end();
-  }
-};
+// 404 page
+
 const server = http.createServer(requestHandler);
 server.listen(3001);
